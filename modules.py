f8df41c63ba98f03ad912f9a849e4982dc65d136
immertss <imuharremmertt@gmail.com>
2025-02-20 18:37:32 +0300

    pybasic1

---
+++ modules.py
@@ -0,0 +1,12 @@
+import pytemel33 as p33
+from pytemel33 import bol
+import random
+import PythonTeemeller2 
+from PythonTeemeller2 import sayhello,Human
+from selenium import webdriver
+
+print(random.randint(0,100))
+print(p33.bol(10,5))
+human1=Human("Merdo")
+
+#packages
